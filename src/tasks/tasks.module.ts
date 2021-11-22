cddf1809b935e4b4c051710ae7c6eb1ec45e672f
Dana Gajewska <DanaGajewska@rocketinnovationstudio.ca>
2021-11-22 15:26:50 -0500

    Fixes formatting

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { Task } from './dto/task.entity';
 import { TasksController } from './tasks.controller';
 import { TaskRepository } from './tasks.repository';
 import { TasksService } from './tasks.service';
@@ -8,6 +7,6 @@ import { TasksService } from './tasks.service';
 @Module({
   imports: [TypeOrmModule.forFeature([TaskRepository])],
   controllers: [TasksController],
-  providers: [TasksService]
+  providers: [TasksService],
 })
 export class TasksModule {}
