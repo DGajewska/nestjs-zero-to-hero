0139e162a29010a0b4256ffdbf121248789320ae
Dana Gajewska <DanaGajewska@rocketinnovationstudio.ca>
2021-11-24 10:21:18 -0500

    Sets up  auth repo, service, controller, entity. Creates first route

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,7 +1,24 @@
+import { ConflictException } from '@nestjs/common';
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
+
+const isDuplicateUsername = (err) => err.code === '23505';
 
 @EntityRepository(User)
 export class UsersRepository extends Repository<User> {
-  //
+  async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+    const user = this.create({ username, password });
+
+    try {
+      await this.save(user);
+    } catch (err) {
+      if (isDuplicateUsername(err)) {
+        throw new ConflictException('Username already exists');
+      } else {
+        throw err;
+      }
+    }
+  }
 }
