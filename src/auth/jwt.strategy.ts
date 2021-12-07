98112491fc9b740538de94b38e62f414afcde570
Dana Gajewska <DanaGajewska@rocketinnovationstudio.ca>
2021-12-07 12:00:30 -0500

    Updates JWT to use config

--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,4 +1,5 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 import { PassportStrategy } from '@nestjs/passport';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ExtractJwt, Strategy } from 'passport-jwt';
@@ -11,15 +12,17 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(
     @InjectRepository(UsersRepository)
     private usersRepository: UsersRepository,
+    private configService: ConfigService,
   ) {
     super({
-      secretOrKey: 'topSecret!1234',
+      secretOrKey: configService.get('JWT_SECRET'),
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
     });
   }
 
   async validate(payload: JwtPayload): Promise<User> {
     const { username } = payload;
+
     const user: User = await this.usersRepository.findOne({ username });
 
     if (!user) {
--- src/config.schema.ts
+++ src/config.schema.ts
@@ -1,10 +1,11 @@
 import * as Joi from 'joi';
 
 export const configValidationSchema = Joi.object({
-  STAGE: Joi.string().allow(['dev', 'prod']).required(),
+  STAGE: Joi.string().allow('dev', 'prod').required(),
   DB_HOST: Joi.string().required(),
   DB_PORT: Joi.number().default(5432).required(),
   DB_USERNAME: Joi.string().required(),
   DB_PASSWORD: Joi.string().required(),
   DB_DATABASE: Joi.string().required(),
+  JWT_SECRET: Joi.string().required(),
 });
